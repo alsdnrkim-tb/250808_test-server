d2d9de8ca96db1c95421d4009c4587cf4dd22ae6
alsdnrkim-tb <alsdnrkim@gmail.com>
2025-08-08 16:29:16 +0900

    Servlet update

--- src/main/java/AIServlet.java
+++ src/main/java/AIServlet.java
@@ -1,4 +1,7 @@
 import com.google.genai.Client;
+import com.google.genai.types.Content;
+import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.Part;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -6,12 +9,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-//import jdk.incubator.foreign.CLinker;
 
 import java.io.IOException;
 
 // [2]
-@WebServlet("/ai") // localhost:8080/ai
+@WebServlet("/") // localhost:8080/ai
 // 이 서블릿을 통해 호출
 public class AIServlet extends HttpServlet { // [1]
     // doGet, doPost
@@ -20,18 +22,52 @@ public class AIServlet extends HttpServlet { // [1]
     @Override // [3]
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Dotenv dotenv = Dotenv.load();
-        String apikey = dotenv.get("GOOGLE_API_KEY");
+        // git에 push할 때 이 dotenv가 push되지 않아서 배포에는 실행이 안됨
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load(); // [7]
+        // .env 파일이 없을 때 에러를 던지는 설정을 하는데 .configure, .ignoreIfMissing를 추가하여 없으면 무시하게 만듦
+        String apiKey = dotenv.get("GOOGLE_API_KEY");
+        // [8] -> resources/.env
         Client client = Client.builder()
-                .apiKey(apikey)
-                .build();
-        String data = client.models.generateContent("gemini-2.0-flash", "요즘 잘나가는 게임 알려주고, 결과만 50자 내로 출력해줘.", null)
-                        .text();
-        req.setAttribute("data", data); // [6]
+                .apiKey(apiKey).build(); // [9]
+        // gemini-2.0-flash
+        String data = client.models.generateContent("gemini-2.0-flash",
+                        "요즘 유행하는 MMORPG 알려주고, 결과만 50자 내로 출력해줘. 마크다운 같이 텍스트를 꾸미는 문법은 제외해.", null)
+                .text(); // text를 불러와줌. [10]
+//        req.setAttribute("data", "안녕하세요! 반갑습니다!"); // [6]
+        req.setAttribute("data", data); // [11]
 
         RequestDispatcher dispatcher = req.getRequestDispatcher(
                 "/WEB-INF/ai.jsp");
         dispatcher.forward(req, resp); // [4]
         // WEB-INF -> ai.jsp [5]
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // [3]
+        // input/post -> paramter
+        String question = req.getParameter("question");
+        // [2]
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load(); // [7]
+        // .env 파일이 없을 때 에러를 던지는 설정을 하는데 .configure, .ignoreIfMissing를 추가하여 없으면 무시하게 만듦
+        String apiKey = dotenv.get("GOOGLE_API_KEY");
+        Client client = Client.builder()
+                .apiKey(apiKey).build();
+        String data = client.models.generateContent("gemini-2.0-flash",
+                        question, GenerateContentConfig.builder().systemInstruction(Content.builder()
+                                .parts(Part.builder().text(
+                                        "마크다운 없이 평문으로."))).build()) // [4]
+                .text(); // text를 불러와줌.
+        req.setAttribute("data", data);
+        req.setAttribute("question", question); // input에다 넣었던 것을
+        // attribute로 재전달 [5]
+        // [1]
+        RequestDispatcher dispatcher = req.getRequestDispatcher(
+                "/WEB-INF/ai.jsp");
+        dispatcher.forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/ai.jsp
+++ src/main/webapp/WEB-INF/ai.jsp
@@ -1,16 +1,37 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <html>
 <head>
-    <title>게임 너무 좋아</title>
-    </head>
-<body>
+    <title>게임 추천</title>
+    <!-- OG Tag -->
+    <meta property="og:tatle" content="예시"
+    <meta property="og:description" content="예시2">
+    <style>
+    @font-face {
+        font-family: 'TTSamlipCreamyWhiteR';
+        src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2408-4@1.0/TTSamlipCreamyWhiteR.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+     body {
+                font-family: 'TTSamlipCreamyWhiteR';
+            }
+    </style>
 
+</head>
+
+<body>
+<!-- II. 질문을 표시 -->
 <p>
-    <!-- req 안에 set된 data라는 attribute가 있따면 출력 -->
-    <%= request.getAttribute("data") %>
+    질문 : <%= request.getAttribute("question") %>
 </p>
 <p>
-    이름 : <%= request.getAttribute("name") %>
+    <!-- 2. req(request) 안에 set된 'data'라는 attribute가 있다면 출력 -->
+    답변 : <%= request.getAttribute("data") %> <!-- 값을 출력 -->
 </p>
+<!-- I. 주소창이 아니라 우리가 데이터를 화면에서 아예 전달 -->
+<form method="post">
+    <input name="question">
+    <button>질문하기</button>
+</form>
 </body>
 </html>
